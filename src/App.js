482602224ae7e840f82e3de2a8ca282341f67c38
maxditamo <103543208+maxditamo@users.noreply.github.com>
2022-08-20 15:47:30 -0300

    version Clase 4 end

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ const estilos = {
   padding: '20px'
 }
 
+const log = () => { console.log('Hola');}
+const log2 = () => { console.log('Hi');}
+
+
   return (
     <div className='conteiner'>
     <h1>¡Hola Mundo!</h1>
     <strong>Otro mensaje</strong>
     <h3 style= { estilos }>{ mensaje } </h3>
-    <Saludo name='Juan' lastname='Perez'/>
+    <Saludo name='Juan' lastname='Perez' fn={log}> 
+    <p>Mensaje 1</p>
+    <p>Mensaje 2</p>
+    <p>Mensaje 3</p>
+    </Saludo>
+    <Saludo name='Ana' lastname='Lopez' fn={log2}>
+    <li>Un elemento de lista</li>
+    </Saludo>
+    <Saludo name='Miguel' lastname='Gonzalez'>
+    </Saludo>
     </div>
   );
 }
--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,6 +1,13 @@
-const Saludo = ( {name, lastname} ) => {
+const Saludo = ( {name, lastname, fn, children} ) => {
+    
+    if (fn){
+        fn()
+    }
     return (
-        <h1>¡Saludos {name} {lastname}!</h1>
+        <>
+        <h1>¡Saludos a: {name} {lastname}!</h1> 
+        {children}
+        </>
     )
 }
 
