e8a4a1b70dcdec2fa110b3a997f8204cc96f8f9d
maxditamo <103543208+maxditamo@users.noreply.github.com>
2022-08-20 13:45:25 -0300

    version clase 3 & 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+const mensaje = 'Bienvenido a mi app'
+const estilos = {
+  backgroundColor: '#888'
+}
+
   return (
-    <>
+    <div className='conteiner'>
     <h1>¡Hola Mundo!</h1>
-    <h3>Lo logre!!</h3>
-    </>
+    <h3>style= { estilos } { mensaje } </h3>
+    </div>
   );
 }
 
